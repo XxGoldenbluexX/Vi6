25d82842ebf1e57cef6d7d8abeeadee5fb8df8d2
XxGoldenbluexX <tonyg222@outlook.fr>
2021-01-22 13:05:50 +0100

    PlayerGameStats description

--- src/main/java/fr/nekotine/vi6/sql/PlayerGameStats.java
+++ src/main/java/fr/nekotine/vi6/sql/PlayerGameStats.java
@@ -1,4 +1,12 @@
 package fr.nekotine.vi6.sql;
 
+/**
+ * Made from Game, this class is used to move information to the SQLInterface
+ * 
+ * @author XxGoldenbluexX
+ * @author Hyez
+ *
+ */
+
 public class PlayerGameStats {
 }
