2a1b0204436f456010683fc4c4bd7639d7053993
XxGoldenbluexX <tonyg222@outlook.fr>
2021-01-22 11:41:26 +0100

    SQL classes created

---
+++ src/main/java/fr/nekotine/vi6/sql/PlayerGameStats.java
@@ -0,0 +1,4 @@
+package fr.nekotine.vi6.sql;
+
+public class PlayerGameStats {
+}
---
+++ src/main/java/fr/nekotine/vi6/sql/SQLInterface.java
@@ -0,0 +1,24 @@
+package fr.nekotine.vi6.sql;
+
+/**
+ * This class provide severeal methods to add/remove/modify stats from the SQL database.
+ * 
+ * @author XxGoldenbluexX
+ * @author Hyez
+ *
+ */
+
+public class SQLInterface {
+	
+	/**
+	 * Add player stats for a specified game to the SQL database.
+	 * 
+	 * @param gameId ID of the game played.
+	 * @param pStats Stats of the player in this game.
+	 * @return return whether yes if stats was succefully added, else false.
+	 */
+	public boolean addPlayerGameStats(int gameId, PlayerGameStats pStats) {
+		return false;
+	}
+	
+}
