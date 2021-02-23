c916d32d2f33c01a51490c0014123bf42e485350
XxGoldenbluexX <tonyg222@outlook.fr>
2021-02-23 20:15:34 +0100

    removed unused imports

--- src/main/java/fr/nekotine/vi6/statuseffects/StatusEffect.java
+++ src/main/java/fr/nekotine/vi6/statuseffects/StatusEffect.java
@@ -1,9 +1,6 @@
 package fr.nekotine.vi6.statuseffects;
 
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import fr.nekotine.vi6.wrappers.PlayerWrapper;
@@ -24,7 +21,6 @@ public class StatusEffect {
 	public void setWrapper(PlayerWrapper w) {
 		if (wrapper!=null) remove();
 		wrapper=w;
-		Player p;
 	}
 	
 	public void remove() {
