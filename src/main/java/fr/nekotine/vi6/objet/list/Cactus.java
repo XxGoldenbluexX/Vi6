3bce6fbee9e3dcdafcb54eb3e26438e2837c3d80
XxGoldenbluexX <tonyg222@outlook.fr>
2021-03-04 10:37:04 +0100

    adding sting sound to cactus

--- src/main/java/fr/nekotine/vi6/objet/list/Cactus.java
+++ src/main/java/fr/nekotine/vi6/objet/list/Cactus.java
@@ -2,10 +2,14 @@ package fr.nekotine.vi6.objet.list;
 
 import java.util.Map.Entry;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
 
 import fr.nekotine.vi6.Game;
 import fr.nekotine.vi6.Vi6Main;
@@ -16,9 +20,13 @@ import fr.nekotine.vi6.objet.ObjetsSkins;
 import fr.nekotine.vi6.objet.utils.Objet;
 import fr.nekotine.vi6.utils.IsCreator;
 import fr.nekotine.vi6.wrappers.PlayerWrapper;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
 
 public class Cactus extends Objet{
 
+	private static final ItemStack CATCUS = IsCreator.createItemStack(Material.CACTUS, 1, ChatColor.DARK_GREEN+"Cactus");
+	
 	public Cactus(Vi6Main main, ObjetsList objet, ObjetsSkins skin, Player player, Game game) {
 		super(main, objet, skin, IsCreator.createObjetItemStack(main,objet,1), game, player);
 	}
@@ -37,10 +45,12 @@ public class Cactus extends Objet{
 
 	@Override
 	public void death(Player holder) {
+		holder.getInventory().remove(CATCUS);
 	}
 
 	@Override
 	public void sell(Player holder) {
+		holder.getInventory().remove(CATCUS);
 	}
 
 	@Override
@@ -58,7 +68,10 @@ public class Cactus extends Objet{
 		game.getWrapper((Player)e.getEntity()).getTeam()==Team.VOLEUR) {
 			for(Entry<Player, PlayerWrapper> player : game.getPlayerMap().entrySet()) {
 				if(player.getValue().getTeam()==Team.VOLEUR && player.getValue().getState()==PlayerState.INSIDE) {
-					player.getKey().damage(0.01);
+					Player p = player.getKey();
+					p.damage(0.01);
+					Location loc = p.getLocation();
+					loc.getWorld().playSound(Sound.sound(Key.key("entity.bee.sting"),Sound.Source.AMBIENT,1f,1f), loc.getX(), loc.getY(), loc.getZ());
 				}
 			}
 		}
