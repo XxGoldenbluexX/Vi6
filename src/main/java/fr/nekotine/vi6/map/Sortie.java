7347f93f0bb8dfff6062e56349849da5118a849b
XxGoldenbluexX <tonyg222@outlook.fr>
2021-03-13 15:06:56 +0100

    MAJ OBJET: Sortie

--- src/main/java/fr/nekotine/vi6/map/Sortie.java
+++ src/main/java/fr/nekotine/vi6/map/Sortie.java
@@ -1,15 +1,5 @@
 package fr.nekotine.vi6.map;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.bukkit.GameMode;
-import org.bukkit.configuration.serialization.ConfigurationSerializable;
-import org.bukkit.configuration.serialization.SerializableAs;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-
 import fr.nekotine.vi6.Vi6Main;
 import fr.nekotine.vi6.enums.PlayerState;
 import fr.nekotine.vi6.enums.Team;
@@ -19,37 +9,46 @@ import fr.nekotine.vi6.utils.MessageFormater;
 import fr.nekotine.vi6.utils.ZoneDetectionListener;
 import fr.nekotine.vi6.wrappers.PlayerWrapper;
 import fr.nekotine.vi6.yml.DisplayTexts;
+import java.util.HashMap;
+import java.util.Map;
+import net.kyori.adventure.text.Component;
+import org.bukkit.GameMode;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.configuration.serialization.SerializableAs;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 @SerializableAs("Sortie")
-public class Sortie implements ConfigurationSerializable,ZoneDetectionListener {
-	
-	private static final String yamlPrefix = "Sortie_";
-	
+public class Sortie implements ConfigurationSerializable, ZoneDetectionListener {
+	private static final String YAML_PREFIX = "Sortie_";
+
 	private String name;
+
 	private String displayName;
+
 	private DetectionZone zone;
-	
-	public Sortie(String name,String displayName, DetectionZone z) {
-		this.name=name;
-		this.displayName=displayName;
+
+	public Sortie(String name, String displayName, DetectionZone z) {
+		this.name = name;
+		this.displayName = displayName;
 		setZone(z);
 	}
 
-	@Override
 	public Map<String, Object> serialize() {
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		map.put("name", name);
-		map.put("displayName", displayName);
-		map.put("zone", zone);
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("name", this.name);
+		map.put("displayName", this.displayName);
+		map.put("zone", this.zone);
 		return map;
 	}
-	
+
 	public static Sortie deserialize(Map<String, Object> args) {
-		return new Sortie((String)args.get("name"),(String)args.get("displayName"),(DetectionZone)args.get("zone"));
+		return new Sortie((String) args.get("name"), (String) args.get("displayName"),
+				(DetectionZone) args.get("zone"));
 	}
 
 	public String getDisplayName() {
-		return displayName;
+		return this.displayName;
 	}
 
 	public void setDisplayName(String displayName) {
@@ -57,65 +56,63 @@ public class Sortie implements ConfigurationSerializable,ZoneDetectionListener {
 	}
 
 	public DetectionZone getZone() {
-		return zone;
+		return this.zone;
 	}
 
 	public void setZone(DetectionZone zone) {
-		if (this.zone!=null) {
+		if (this.zone != null)
 			this.zone.removeListener(this);
-		}
 		this.zone = zone;
 		this.zone.addListener(this);
-		
-	}
-	
-	public void destroy() {
-		zone.destroy();
-	}
-	
-	public void enable(Vi6Main mainref) {
-		zone.enable(mainref);
-		zone.addListener(this);
 	}
 
-	@Override
-	public boolean playerEnterZone(Player player,DetectionZone zone,Vi6Main mainref) {
+	public void destroy() {
+		this.zone.destroy();
+	}
+
+	public void enable(Vi6Main mainref) {
+		this.zone.enable(mainref);
+		this.zone.addListener(this);
+	}
+
+	public boolean playerEnterZone(Player player, DetectionZone zone, Vi6Main mainref) {
 		PlayerWrapper wrap = mainref.getPlayerWrapper(player);
-		if(wrap!=null && wrap.getTeam()==Team.VOLEUR && wrap.getState()==PlayerState.INSIDE && wrap.isCanEscape()) {
+		if (wrap != null && wrap.getTeam() == Team.VOLEUR && wrap.getState() == PlayerState.INSIDE
+				&& wrap.isCanEscape()) {
 			wrap.setState(PlayerState.LEAVED);
 			player.setGameMode(GameMode.SPECTATOR);
-			for(ItemStack itm : player.getInventory().getContents()) {
-				if(itm!=null) {
+			for (ItemStack itm : player.getInventory().getContents()) {
+				if (itm != null) {
 					Objet objet = wrap.getGame().getObjet(itm);
-					if(objet!=null) objet.leaveMap(player);
+					if (objet != null)
+						objet.leaveMap();
 				}
 			}
-			for(Entry<Player, PlayerWrapper> p : wrap.getGame().getPlayerMap().entrySet()) {
-				p.getKey().sendMessage(MessageFormater.formatWithColorCodes('§',DisplayTexts.getMessage("game_player_escaped"),
-						new MessageFormater("§p", player.getName()),new MessageFormater("§n", String.valueOf(wrap.getStealedArtefactList().size()))));
+			for (Map.Entry<Player, PlayerWrapper> p : wrap.getGame().getPlayerMap().entrySet()) {
+				((Player) p.getKey()).sendMessage((Component) MessageFormater.formatWithColorCodes('§',
+						DisplayTexts.getMessage("game_player_escaped"),
+						new MessageFormater[]{new MessageFormater("§p", player.getName()),
+								new MessageFormater("§n", String.valueOf(wrap.getStealedArtefactList().size()))}));
 			}
-			if(!wrap.getGame().isThiefLeft()) {
+			if (!wrap.getGame().isThiefLeft())
 				wrap.getGame().endGame(false);
-			}
 		}
 		return false;
 	}
 
-	@Override
-	public boolean playerLeaveZone(Player player,DetectionZone zone,Vi6Main mainref) {
+	public boolean playerLeaveZone(Player player, DetectionZone zone, Vi6Main mainref) {
 		return false;
 	}
-	
+
 	public static final String getYamlPrefix() {
-		return yamlPrefix;
+		return YAML_PREFIX;
 	}
 
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
-
 }
