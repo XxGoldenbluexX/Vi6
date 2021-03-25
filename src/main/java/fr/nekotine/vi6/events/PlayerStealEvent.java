3e7060fd9d0527aa940b59f80789823fc04cc8d4
Hy3z <axel.vivenot@outlook.fr>
2021-03-25 12:44:06 +0100

    globe voyant début

--- src/main/java/fr/nekotine/vi6/events/PlayerStealEvent.java
+++ src/main/java/fr/nekotine/vi6/events/PlayerStealEvent.java
@@ -4,11 +4,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+import fr.nekotine.vi6.Game;
 import fr.nekotine.vi6.map.Artefact;
 
 public class PlayerStealEvent extends Event{
 	private final Artefact artefact;
 	private final Player player;
+	private final Game game;
 	private static final HandlerList handlers = new HandlerList();
 	public static HandlerList getHandlerList() {
 	    return handlers;
@@ -17,15 +19,18 @@ public class PlayerStealEvent extends Event{
 	public HandlerList getHandlers() {
 		return handlers;
 	}
-	public PlayerStealEvent(Player player, Artefact artefact) {
+	public PlayerStealEvent(Player player, Artefact artefact, Game game) {
 		this.artefact = artefact;
 		this.player = player;
-		
+		this.game = game;
 	}
 	public Artefact getArtefact() {
 		return artefact;
 	}
 	public Player getPlayer() {
 		return player;
+	}
+	public Game getGame() {
+		return game;
 	}
 }
--- src/main/java/fr/nekotine/vi6/objet/list/GlobeVoyant.java
+++ src/main/java/fr/nekotine/vi6/objet/list/GlobeVoyant.java
@@ -1,10 +1,15 @@
 package fr.nekotine.vi6.objet.list;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import fr.nekotine.vi6.Game;
 import fr.nekotine.vi6.Vi6Main;
+import fr.nekotine.vi6.events.PlayerStealEvent;
+import fr.nekotine.vi6.map.Artefact;
+import fr.nekotine.vi6.map.Artefact.CaptureState;
 import fr.nekotine.vi6.objet.ObjetsList;
 import fr.nekotine.vi6.objet.ObjetsSkins;
 import fr.nekotine.vi6.objet.utils.Objet;
@@ -12,6 +17,7 @@ import fr.nekotine.vi6.wrappers.PlayerWrapper;
 
 public class GlobeVoyant extends Objet{
 	private static final int MESSAGE_DELAY_TICKS=100;
+	private Artefact attached;
 	public GlobeVoyant(Vi6Main main, ObjetsList objet, ObjetsSkins skin, Game game, Player player,
 			PlayerWrapper wrapper) {
 		super(main, objet, skin, game, player, wrapper);
@@ -35,6 +41,26 @@ public class GlobeVoyant extends Objet{
 
 	@Override
 	public void action(Action var1) {
+		if(var1!=Action.PHYSICAL) {
+			double nearestD = 0;
+			Artefact nearestA = null;
+			for(Artefact artefact : getGame().getMap().getArtefactList()) {
+				if(artefact.getStatus()==CaptureState.STEALABLE) {
+					double distance = getOwner().getLocation().distanceSquared(artefact.getBlockLoc());
+					if(nearestA==null || distance<nearestD) {
+						nearestA=artefact;
+						nearestD=distance;
+					}
+				}
+			}
+			if(nearestA!=null) {
+				attached=nearestA;
+				//message
+			}else {
+				//message tout est pris ou pas aurtefact
+			}
+			consume();
+		}
 	}
 
 	@Override
@@ -44,5 +70,16 @@ public class GlobeVoyant extends Objet{
 	public static int getMessageDelayTicks() {
 		return MESSAGE_DELAY_TICKS;
 	}
-
+	@EventHandler
+	public void artefactSteal(PlayerStealEvent e) {
+		if(e.getGame().equals(getGame()) && e.getArtefact().equals(attached)) {
+			new BukkitRunnable() {
+				@Override
+				public void run() {
+					//message
+				}
+			}.runTaskLater(getMain(), MESSAGE_DELAY_TICKS);
+			destroy();
+		}
+	}
 }
