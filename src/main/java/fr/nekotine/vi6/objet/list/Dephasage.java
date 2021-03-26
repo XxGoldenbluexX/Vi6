899ff92a6394f2c8c6388f22ad8c522c88c33417
Hy3z <axel.vivenot@outlook.fr>
2021-03-26 19:47:13 +0100

    dephasage sound

--- src/main/java/fr/nekotine/vi6/objet/list/Dephasage.java
+++ src/main/java/fr/nekotine/vi6/objet/list/Dephasage.java
@@ -12,11 +12,14 @@ import fr.nekotine.vi6.objet.utils.Objet;
 import fr.nekotine.vi6.statuseffects.Effects;
 import fr.nekotine.vi6.statuseffects.StatusEffect;
 import fr.nekotine.vi6.wrappers.PlayerWrapper;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
 
 public class Dephasage extends Objet{
 	private static final int DELAY_BETWEEN_INVISIBILITY_TICKS=400;
 	private static final int INVISIBILITY_DURATION_TICKS=40;
-	private int delayBetweenInvisibility=DELAY_BETWEEN_INVISIBILITY_TICKS;
+	private static final int DELAY_BETWEEN_WARNING_SOUND=10;
+	private int delayBetweenInvisibility=DELAY_BETWEEN_INVISIBILITY_TICKS-INVISIBILITY_DURATION_TICKS-1;
 	private StatusEffect invisibilityEffect = new StatusEffect(Effects.Invisible);
 	public Dephasage(Vi6Main main, ObjetsList objet, ObjetsSkins skin, Game game, Player player,
 			PlayerWrapper wrapper) {
@@ -29,7 +32,21 @@ public class Dephasage extends Objet{
 		ItemStack item = super.getDisplayedItem().clone();
 		item.setAmount(delayBetweenInvisibility/20+1);
 		setItem(item);
-		if(delayBetweenInvisibility==0) {
+		if(delayBetweenInvisibility+INVISIBILITY_DURATION_TICKS==DELAY_BETWEEN_INVISIBILITY_TICKS+DELAY_BETWEEN_WARNING_SOUND*2) {
+			super.getOwner().playSound(Sound.sound(Key.key("block.note_block.chime"), Sound.Source.VOICE, 1, 2));
+		}else if(delayBetweenInvisibility+INVISIBILITY_DURATION_TICKS==DELAY_BETWEEN_INVISIBILITY_TICKS+DELAY_BETWEEN_WARNING_SOUND) {
+			super.getOwner().playSound(Sound.sound(Key.key("block.note_block.chime"), Sound.Source.VOICE, 1, 1.5f));
+		}else if(delayBetweenInvisibility+INVISIBILITY_DURATION_TICKS==DELAY_BETWEEN_INVISIBILITY_TICKS) {
+			super.getOwner().playSound(Sound.sound(Key.key("block.note_block.chime"), Sound.Source.VOICE, 1, 1));
+			super.getOwner().playSound(Sound.sound(Key.key("block.beacon.deactivate"), Sound.Source.VOICE, 1, 1.5f));
+		}else if(delayBetweenInvisibility==DELAY_BETWEEN_WARNING_SOUND*2) {
+			super.getOwner().playSound(Sound.sound(Key.key("block.note_block.chime"), Sound.Source.VOICE, 1, 1));
+		}else if(delayBetweenInvisibility==DELAY_BETWEEN_WARNING_SOUND) {
+			super.getOwner().playSound(Sound.sound(Key.key("block.note_block.chime"), Sound.Source.VOICE, 1, 1.5f));
+		}else if(delayBetweenInvisibility==0) {
+			super.getOwner().playSound(Sound.sound(Key.key("block.note_block.chime"), Sound.Source.VOICE, 1, 2));
+			super.getOwner().playSound(Sound.sound(Key.key("block.beacon.activate"), Sound.Source.VOICE, 1, 1.5f));
+			super.getOwner().playSound(Sound.sound(Key.key("entity.illusioner.prepare_blindness"), Sound.Source.VOICE, 1, 1));
 			delayBetweenInvisibility=DELAY_BETWEEN_INVISIBILITY_TICKS;
 			getOwnerWrapper().addStatusEffect(invisibilityEffect);
 			invisibilityEffect.autoRemove(super.getMain(), INVISIBILITY_DURATION_TICKS);
