49e6fa5e3d0a457475883c00819342ff686c678b
XxGoldenbluexX <tonyg222@outlook.fr>
2021-03-26 17:38:44 +0100

    pre-made PiègeADents

---
+++ src/main/java/fr/nekotine/vi6/objet/list/PiegeADents.java
@@ -0,0 +1,59 @@
+package fr.nekotine.vi6.objet.list;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+
+import fr.nekotine.vi6.Game;
+import fr.nekotine.vi6.Vi6Main;
+import fr.nekotine.vi6.objet.ObjetsList;
+import fr.nekotine.vi6.objet.ObjetsSkins;
+import fr.nekotine.vi6.objet.utils.Objet;
+import fr.nekotine.vi6.wrappers.PlayerWrapper;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
+
+public class PiegeADents extends Objet {
+
+	private Player victim;
+	
+	public PiegeADents(Vi6Main main, ObjetsList objet, ObjetsSkins skin, Game game, Player player,PlayerWrapper wrapper) {
+		super(main, objet, skin, game, player, wrapper);
+	}
+
+	@Override
+	public void tick() {
+	}
+
+	@Override
+	public void cooldownEnded() {
+	}
+
+	@Override
+	public void death() {
+	}
+
+	@Override
+	public void leaveMap() {
+	}
+
+	@Override
+	public void action(Action var1) {
+	}
+
+	@Override
+	public void drop() {
+	}
+	
+	private void cast() {
+		if (!onGround()) {
+			getOwner().playSound(Sound.sound(Key.key("entity.villager.no"), Sound.Source.AMBIENT, 1.0F, 1.0F));
+			return;
+		}
+	}
+	
+	private boolean onGround() {
+		return (!getOwner().isFlying()
+				&& getOwner().getLocation().subtract(0.0D, 0.1D, 0.0D).getBlock().getType().isSolid());
+	}
+
+}
