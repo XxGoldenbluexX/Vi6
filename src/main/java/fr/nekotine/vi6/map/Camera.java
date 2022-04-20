0cc4aaf9b6483836ea06770c82d0b5f7ace708cf
Hy3z <axel.vivenot@outlook.fr>
2022-04-20 19:55:44 +0200

    se faire jam dans la camera te la fait quitter

--- src/main/java/fr/nekotine/vi6/map/Camera.java
+++ src/main/java/fr/nekotine/vi6/map/Camera.java
@@ -23,6 +23,7 @@ import org.bukkit.util.EulerAngle;
 import fr.nekotine.vi6.Game;
 import fr.nekotine.vi6.Vi6Main;
 import fr.nekotine.vi6.enums.Team;
+import fr.nekotine.vi6.events.PlayerJamEvent;
 import fr.nekotine.vi6.events.PlayerLeaveCamera;
 import fr.nekotine.vi6.events.PlayerScanEvent;
 import fr.nekotine.vi6.utils.CameraState;
@@ -62,6 +63,12 @@ public class Camera implements ConfigurationSerializable, Listener{
 		idleHead = IsCreator.createSkull(idleURL);
 		startingHead = IsCreator.createSkull(startingURL);
 		activeHead = IsCreator.createSkull(activeURL);
+	}
+	@EventHandler
+	public void onJam(PlayerJamEvent e) {
+		if(viewers.containsKey(e.getPlayer())) {
+			removeViewer(e.getPlayer());
+		}
 	}
 	@EventHandler
 	public void onScan(PlayerScanEvent e) {
